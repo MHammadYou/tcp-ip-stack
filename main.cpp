c9ef7aa1a22f6b39ead5b5dc5cee8ff46e094705
MHammadYou <MHammadY2.0@gmail.com>
2023-02-03 22:43:30 +0500

    Added Tun interface

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
+#include <iostream>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <iomanip>
+#include <arpa/inet.h>
+#include <cstdlib>
 
+const char* TUN_DEVICE = "/dev/net/tun";
+const char* TUN_NAME = "tun0";
 
 int main()
 {
-    
-}
+
+    int tun_fd = open(TUN_DEVICE, O_RDWR);
+
+    if (tun_fd < 0)
+    {
+        std::cerr << "Failed to open tun interface" << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+
+    ifr.ifr_flags = IFF_TUN;
+    strncpy(ifr.ifr_name, TUN_NAME, IFNAMSIZ);
+
+    if (ioctl(tun_fd, TUNSETIFF, &ifr) < 0)
+    {
+        perror("Failed to configure tun interface");
+        return 1;
+    }
+
+    uint8_t buffer[2048];
+    int nbytes = read(tun_fd, buffer, sizeof(buffer));
+
+    if (nbytes < 0)
+    {
+        perror("Failed to read from tun interface");
+        return 1;
+    }
+
+    uint16_t protocol = (buffer[2] << 8) | buffer[3];
+    std::cout << std::hex << protocol << std::endl;
+
+    close(tun_fd);
+    return 0;
+}   
